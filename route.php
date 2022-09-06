c6d352fefe15cf7acbf2f3256b7530ef80605d22
MorelK1 <kouhossounon.vianney@gmail.com>
2022-09-06 22:38:27 +0100

    changement du fichier route pour afficher la page de connexion

--- route.php
+++ route.php
@@ -31,10 +31,10 @@ if(isset($_GET["p"])){
             }
         }
     }
-else require_once (ROOT . "views/accueil/log.php");
+    else require_once (ROOT . "views/dash.php");
 }
 
-else require_once(ROOT . "views/dash.php");
+else require_once(ROOT . "views/accueil/log.php");
 
 
 
