a1f5b791c331e1b94694334dc71dd17c2d1156f6
MorelK1 <kouhossounon.vianney@gmail.com>
2022-09-07 10:03:25 +0100

    affichage de la page de connexion en mettant juste route.php sans parametre

--- route.php
+++ route.php
@@ -31,7 +31,9 @@ if(isset($_GET["p"])){
             }
         }
     }
-    else require_once (ROOT . "views/dash.php");
+//    else {
+//        require_once
+//    }
 }
 
 else require_once(ROOT . "views/accueil/log.php");
