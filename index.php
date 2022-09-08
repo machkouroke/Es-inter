5c4c87886f0d0053aa19f729db81961e7f9019dd
machkouroke <40785379+machkouroke@users.noreply.github.com>
2022-09-08 01:06:10 +0100

    `refactor` Addition of a new routing model based on LOPStudents

--- index.php
+++ index.php
@@ -5,35 +5,60 @@
         require_once($path);
     });
     require_once($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'config\config.php');
-    require_once(ROOT . "core/Model.php");
-    require_once(ROOT . "core/Controller.php");
+    require_once(BASE_DIR . "core/Model.php");
+    use controllers\AuthenticationController;
 
-    //separer les parametres
-    if (isset($_GET["action"])) {
-        if ($_GET["action"] != "local") {
+//    //separer les parametres
+//    if (isset($_GET["action"])) {
+//        if ($_GET["action"] != "local") {
+//
+//            $params = explode(".", $_GET["action"]);
+//            print_r($params);
+//
+//            //verifier si un parametre existe
+//            if ($params[0] != "") {
+//                $controller = ucfirst($params[0]);
+//                $action = $params[1] ?? 'index';
+//
+//                require_once(ROOT . 'controllers'. DIRECTORY_SEPARATOR. $controller . '.php');
+//                $controller = new $controller;
+//                if (method_exists($controller, $action)) {
+//                    unset($params[0]);
+//                    unset($params[1]);
+//                    call_user_func_array([$controller, $action], $params);
+//                } else {
+//                    http_response_code(404);
+//                    echo "Methode inexistante";
+//                }
+//            }
+//        }
+//
+//    } else {
+//        require_once(ROOT . "views/accueil/log.php");
+//    }
+    session_start();
+    if (isset($_SESSION['User'])) {
+        if (isset($_GET['action'])) {
+            switch ($_GET['action']) {
+                case 'addStudentPage':
+                    MenuController::addStudent();
+                    break;
 
-            $params = explode(".", $_GET["action"]);
-
-            //verifier si un parametre existe
-            if ($params[0] != "") {
-                $controller = ucfirst($params[0]);
-                $action = $params[1] ?? 'index';
-
-                require_once(ROOT . 'controllers/' . $controller . '.php');
-                $controller = new $controller;
-                if (method_exists($controller, $action)) {
-                    unset($params[0]);
-                    unset($params[1]);
-                    call_user_func_array([$controller, $action], $params);
-                } else {
-                    http_response_code(404);
-                    echo "Methode inexistante";
-                }
+                default:
+                    MenuController::menu();
             }
+
+        } else {
+            MenuController::menu();
+        }
+    } else {
+        if (isset($_GET['action']) && $_GET['action'] == 'login') {
+            AuthenticationController::login();
+
+        } else {
+            AuthenticationController::loginPage();
         }
 
-    } else {
-        require_once(ROOT . "views/accueil/log.php");
     }
 
 
