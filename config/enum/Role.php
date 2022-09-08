7052e2b0e2fbb358497d9528fa37670dca31ff75
machkouroke <40785379+machkouroke@users.noreply.github.com>
2022-09-08 01:59:22 +0100

    `refactor` Renaming `Role::Employe` to `Role::Employee`

---
+++ config/enum/Role.php
@@ -0,0 +1,13 @@
+<?php
+
+
+    namespace config\enum;
+    /**
+     * Enumeration sur la fonction des utilisateurs
+     */
+    enum Role: string
+    {
+        case Admin = 'admin';
+        case Employee = 'employee';
+
+    }
